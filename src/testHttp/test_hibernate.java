d77f3d99e06b4ca73af445c8705eeec215e0eab9
zhangkl <335627901@qq.com>
2016-04-18 09:48:28 +0800

    2016-04-08

---
+++ src/testHttp/test_hibernate.java
@@ -0,0 +1,11 @@
+package testHttp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangkl
+ * Date: 2016/3/21
+ * Time: 17:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class test_hibernate {
+}
